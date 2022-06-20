792e9dd65f9929f29c3ae6fe077c34257c87f0b4
zhaoleihe <zhaoleihe@bytedance.com>
2022-06-20 21:44:42 +0800

    无限循环viewpager2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.util.Log
 import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
@@ -12,8 +14,9 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
 
-    var viewPager: IFNViewPager? = null
+    var viewPager: IFNViewPager2? = null
 
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,33 +29,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewPager = findViewById(R.id.infini_view_pager)
-        viewPager?.adapter = object : IFNAdapter() {
+        viewPager?.windowFactory = object : IFNViewPager2.WindowFactory {
 
-            var index = 0
-            override fun instantiateItem(container: ViewGroup): Any {
-                val inflate = View.inflate(this@MainActivity, R.layout.layout_item, null)
-                inflate.findViewById<ImageView>(R.id.iv)?.apply {
-                    setImageResource(res[index % res.size])
-                    index++
-                }
-                inflate.findViewById<TextView>(R.id.tv)?.apply {
-                    text = "Surface:${this.hashCode() / 10000}"
-                }
-                container.addView(
-                    inflate,
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                return inflate
-            }
-
-            override fun isViewFromObject(view: View, any: Any): Boolean {
-                return view == any
-            }
-
-            override fun destroyItem(container: ViewGroup, position: Int, any: Any) {
-                container.removeView(any as View)
-            }
+            var tempIndex = 0
 
             val res = arrayListOf(
                 R.drawable.bg0,
@@ -66,7 +45,54 @@ class MainActivity : AppCompatActivity() {
                 R.drawable.bg8,
                 R.drawable.bg9
             )
-        }
 
+            override fun buildWindow(): InfiniWindow {
+
+                return object : InfiniWindow() {
+                    override fun onCreateView(): View {
+                        val inflate = View.inflate(this@MainActivity, R.layout.layout_item, null)
+                        inflate.findViewById<TextView>(R.id.tv)?.apply {
+                            when (tempIndex) {
+                                0 -> {
+                                    text = "Surface:A"
+                                }
+                                1 -> {
+                                    text = "Surface:B"
+                                }
+                                2 -> {
+                                    text = "Surface:C"
+                                }
+                            }
+                        }
+                        tempIndex++
+                        Log.e(TAG, "onCreateView: $tempIndex")
+                        return inflate
+                    }
+
+                    override fun onBindData(position: Float) {
+                        super.onBindData(position)
+                        if (position > 0) {
+                            rootView?.findViewById<ImageView>(R.id.iv)?.apply {
+                                setImageResource(res[position.toInt() % res.size])
+                            }
+                        }
+                        Log.e(TAG, "onBindData: $position")
+                    }
+
+                    override fun onActive() {
+                        rootView?.findViewById<TextView>(R.id.tv)?.apply {
+                            setTextColor(Color.RED)
+                        }
+                    }
+
+                    override fun onUnActive() {
+                        rootView?.findViewById<TextView>(R.id.tv)?.apply {
+                            setTextColor(Color.GRAY)
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
